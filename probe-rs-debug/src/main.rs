b0c6d7b0fd3a871d6d1a7b8825f5bdab54f224fc
David Watson <david@neonquill.com>
2022-06-26 22:00:10 -0400

    Success! I'm able to program the chip!!!!!!

--- probe-rs-debug/src/main.rs
+++ probe-rs-debug/src/main.rs
@@ -33,6 +33,11 @@ impl Atsaml10 {
     const _DSU_DID_ADDR: u32 = Self::DSU_ADDR + 0x18;
     const DSU_BCC0_ADDR: u32 = Self::DSU_ADDR + 0x20;
     const DSU_BCC1_ADDR: u32 = Self::DSU_ADDR + 0x24;
+    const NVMCTRL_ADDR: u32 = 0x41004000;
+    const NVMCTRL_CTRLA_ADDR: u32 = Self::NVMCTRL_ADDR + 0x00;
+    const NVMCTRL_CTRLC_ADDR: u32 = Self::NVMCTRL_ADDR + 0x08;
+    const NVMCTRL_STATUS_ADDR: u32 = Self::NVMCTRL_ADDR + 0x18;
+    const NVMCTRL_ADDR_ADDR: u32 = Self::NVMCTRL_ADDR + 0x1C;
     // XXX Handle registers better.
     const CRSTEXT_BIT: u8 = 1 << 1;
     const BCCD1_BIT: u8 = 1 << 7;
@@ -52,6 +57,13 @@ impl Atsaml10 {
     const _SIG_CMD_VALID: u32 = 0xEC0000_24;
     // Boot ROM ok to exit.
     const SIG_BOOTOK: u32 = 0xEC0000_39;
+    // Flash row size.
+    const ROW_SIZE: u32 = 256;
+    // Erase row command.
+    const NVMCTRL_CTRLA_ER_CMD: u16 = 0xa502;
+    // XXX Overwrite the first two bytes of CTRLB which default to 0...
+    const NVMCTRL_CTRLA_ER_CMD32: u32 = (Self::NVMCTRL_CTRLA_ER_CMD as u32) << 16;
+    const NVMCTRL_STATUS_READY: u8 = 1 << 2;
 
     /// Performs a cold plugging sequence.
     fn do_cold_plug(&self, interface: &mut Box<dyn ArmProbeInterface>) -> Result<()> {
@@ -161,104 +173,102 @@ fn main() -> Result<()> {
         simplelog::ColorChoice::Auto,
     )?;
 
+    let probes = Probe::list_all();
+    let mut probe = probes[0].open()?;
+    // This path doesn't work.
+    // let interface = probe.try_as_dap_probe()?;
+
+    // This path works, presumably because connecting
+    // causes the pins to switch to outputs.
+    // But now I don't want to attach and do everything automatically.
+    // log::warn!("MANUAL probe.attach");
+    //let mut session = probe.attach("ATSAML10E16A", Permissions::default())?;
+
+    // Attach without running any init routines (?).
+    log::warn!("MANUAL attach_to_unspecified");
+    probe.attach_to_unspecified()?;
+
+    log::warn!("MANUAL try_into_arm_interface");
+    let mut interface = probe.try_into_arm_interface().map_err(|(_, e)| e)?;
+
     /*
-       let probes = Probe::list_all();
-       let mut probe = probes[0].open()?;
-       // This path doesn't work.
-       // let interface = probe.try_as_dap_probe()?;
-
-       // This path works, presumably because connecting
-       // causes the pins to switch to outputs.
-       // But now I don't want to attach and do everything automatically.
-       // log::warn!("MANUAL probe.attach");
-       //let mut session = probe.attach("ATSAML10E16A", Permissions::default())?;
-
-       // Attach without running any init routines (?).
-       log::warn!("MANUAL attach_to_unspecified");
-       probe.attach_to_unspecified()?;
-
-       log::warn!("MANUAL try_into_arm_interface");
-       let mut interface = probe.try_into_arm_interface().map_err(|(_, e)| e)?;
-
-       /*
-           // First, do a cold plug sequence.
-           // XXX Something before this is sending commands...
-           log::warn!("MANUAL cold plug");
-           atsaml10
-               .do_cold_plug(&mut interface)
-               .context("Failed to do cold plug")?;
-       */
-
-       // Don't know how to call this as a function...
-       {
-           let mut pin_out = Pins(0);
-           let mut pin_mask = Pins(0);
-
-           log::warn!("atsaml10 do_cold_plug()");
-
-           // 1 ms with reset high.
-           pin_out.set_nreset(true);
-           pin_mask.set_nreset(true);
-           interface.swj_pins(pin_out.0 as u32, pin_mask.0 as u32, 0)?;
-           thread::sleep(Duration::from_millis(1));
-
-           // 1 ms with reset low.
-           pin_out.set_nreset(false);
-           interface.swj_pins(pin_out.0 as u32, pin_mask.0 as u32, 0)?;
-           thread::sleep(Duration::from_millis(1));
-
-           // 1 ms with reset and clock low.
-           pin_mask.set_swclk_tck(true);
-           interface.swj_pins(pin_out.0 as u32, pin_mask.0 as u32, 0)?;
-           thread::sleep(Duration::from_millis(1));
-
-           // 1 ms with reset high.
-           pin_mask.set_swclk_tck(false);
-           pin_out.set_nreset(true);
-           interface.swj_pins(pin_out.0 as u32, pin_mask.0 as u32, 0)?;
-           thread::sleep(Duration::from_millis(1));
-       }
-
-       log::warn!("MANUAL initialize");
-       let mut interface = interface.initialize_unspecified()?;
-
-       log::warn!("MANUAL port");
-       let port = ApAddress {
-           dp: DpAddress::Default,
-           ap: 0,
-       };
-
-       let default_memory_ap = MemoryAp::new(port);
-
-       /*
-       log::warn!("MANUAL read_raw_ap_register");
-       let val = interface.read_raw_ap_register(port, 0)?;
-       log::warn!("val {}", val);
-       */
-
-       let atsaml10 = Atsaml10(());
-
-       log::warn!("MANUAL memory_interface");
-       // This runs a bunch of commands, but none of them seem to error.
-       let mut memory = interface.memory_interface(default_memory_ap)?;
-
-       // Now follow the CMD_EXIT to Park mode diagram (14-9 page 81).
-       log::warn!("MANUAL exit_reset_extension");
-       atsaml10.exit_reset_extension(&mut memory)?;
-       log::warn!("MANUAL exit_interactive_mode");
-       // By not clearing BREXT here we go into park mode.
-       atsaml10.exit_interactive_mode(&mut memory)?;
-
-       // Make sure the debug access level is 2 (unlocked).
-       log::warn!("MANUAL read DAL");
-       let dal = atsaml10.get_dal(&mut memory)?;
-       if dal != 2 {
-           return Err(anyhow!(
-               "Device is locked. DAL == {} != 2. Try erasing the chip to clear.",
-               dal
-           ));
-       }
+        // First, do a cold plug sequence.
+        // XXX Something before this is sending commands...
+        log::warn!("MANUAL cold plug");
+        atsaml10
+            .do_cold_plug(&mut interface)
+            .context("Failed to do cold plug")?;
     */
+
+    // Don't know how to call this as a function...
+    {
+        let mut pin_out = Pins(0);
+        let mut pin_mask = Pins(0);
+
+        log::warn!("atsaml10 do_cold_plug()");
+
+        // 1 ms with reset high.
+        pin_out.set_nreset(true);
+        pin_mask.set_nreset(true);
+        interface.swj_pins(pin_out.0 as u32, pin_mask.0 as u32, 0)?;
+        thread::sleep(Duration::from_millis(1));
+
+        // 1 ms with reset low.
+        pin_out.set_nreset(false);
+        interface.swj_pins(pin_out.0 as u32, pin_mask.0 as u32, 0)?;
+        thread::sleep(Duration::from_millis(1));
+
+        // 1 ms with reset and clock low.
+        pin_mask.set_swclk_tck(true);
+        interface.swj_pins(pin_out.0 as u32, pin_mask.0 as u32, 0)?;
+        thread::sleep(Duration::from_millis(1));
+
+        // 1 ms with reset high.
+        pin_mask.set_swclk_tck(false);
+        pin_out.set_nreset(true);
+        interface.swj_pins(pin_out.0 as u32, pin_mask.0 as u32, 0)?;
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    log::warn!("MANUAL initialize");
+    let mut interface = interface.initialize_unspecified()?;
+
+    log::warn!("MANUAL port");
+    let port = ApAddress {
+        dp: DpAddress::Default,
+        ap: 0,
+    };
+
+    let default_memory_ap = MemoryAp::new(port);
+
+    /*
+    log::warn!("MANUAL read_raw_ap_register");
+    let val = interface.read_raw_ap_register(port, 0)?;
+    log::warn!("val {}", val);
+    */
+
+    let atsaml10 = Atsaml10(());
+
+    log::warn!("MANUAL memory_interface");
+    // This runs a bunch of commands, but none of them seem to error.
+    let mut memory = interface.memory_interface(default_memory_ap)?;
+
+    // Now follow the CMD_EXIT to Park mode diagram (14-9 page 81).
+    log::warn!("MANUAL exit_reset_extension");
+    atsaml10.exit_reset_extension(&mut memory)?;
+    log::warn!("MANUAL exit_interactive_mode");
+    // By not clearing BREXT here we go into park mode.
+    atsaml10.exit_interactive_mode(&mut memory)?;
+
+    // Make sure the debug access level is 2 (unlocked).
+    log::warn!("MANUAL read DAL");
+    let dal = atsaml10.get_dal(&mut memory)?;
+    if dal != 2 {
+        return Err(anyhow!(
+            "Device is locked. DAL == {} != 2. Try erasing the chip to clear.",
+            dal
+        ));
+    }
 
     // Get the target definition.
     let target = probe_rs::config::get_target_by_name("ATSAML10E16A")?;
@@ -349,13 +359,55 @@ fn main() -> Result<()> {
             }
         }
     }
+    // XXX Probably need to check for segments with holes within a row...
 
+    let row_size: usize = Atsaml10::ROW_SIZE as usize;
+
+    // Actually do the flash.
     for chunk in flash_data {
         let data = &bin_data[chunk.segment_offset as usize..][..chunk.segment_filesize as usize];
         hexdump::hexdump(data);
+
+        let size: u32 = chunk.segment_filesize.try_into()?;
+
+        // Enable automatic writes.
+        memory.write_word_8((Atsaml10::NVMCTRL_CTRLC_ADDR).into(), 0)?;
+
+        let mut addr = chunk.address;
+
+        for row in data.chunks(row_size) {
+            // Set the address.
+            memory.write_word_32((Atsaml10::NVMCTRL_ADDR_ADDR).into(), addr)?;
+
+            // Clear memory row.
+            // XXX Would prefer to write this as a 16 bit addr...
+            memory.write_word_32(
+                (Atsaml10::NVMCTRL_CTRLA_ADDR).into(),
+                Atsaml10::NVMCTRL_CTRLA_ER_CMD32,
+            )?;
+
+            // Wait for the NVM controller to be ready.
+            loop {
+                let status = memory.read_word_8((Atsaml10::NVMCTRL_STATUS_ADDR).into())?;
+                if (status & Atsaml10::NVMCTRL_STATUS_READY) != 0 {
+                    break;
+                }
+            }
+
+            if row.len() < row_size {
+                let mut buf = Vec::with_capacity(row_size);
+                buf.extend_from_slice(row);
+                buf.resize(row_size, 0xff);
+
+                memory.write_8(addr.into(), &buf)?;
+            } else {
+                memory.write_8(addr.into(), row)?;
+            }
+
+            addr += Atsaml10::ROW_SIZE;
+        }
     }
 
-    // Actually do the flash.
     let _opt = DownloadOptions::default();
     // XXX I can't figure out how to create a session...
     // loader.commit(interface, opt)?;
